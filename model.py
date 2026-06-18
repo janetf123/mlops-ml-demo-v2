c3b8a5481196069e4db066261031bedf3fb384fb
Janet Flores <jflores106@yahoo.com>
2026-06-17 19:40:23 -0500

    Added number of estimators

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train model
-clf = RandomForestClassifier()
+clf = RandomForestClassifier(n_estimators=150)
 clf.fit(X_train, y_train)
 
 # Save model
